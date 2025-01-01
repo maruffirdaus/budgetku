ae369e1476cbb7080c2ce58774f285744e79dbbd
Arnoldus Bryan Hendry <bryanhendry12@gmail.com>
2025-01-01 23:31:08 +0700

    update on Transaction,OneTimeTransaction and RecurringTransaction

--- app/src/main/java/app/budgetku/data/local/entity/OneTimeTransaction.java
+++ app/src/main/java/app/budgetku/data/local/entity/OneTimeTransaction.java
@@ -1,4 +1,10 @@
 package app.budgetku.data.local.entity;
 
-public class OneTimeTransaction {
+import androidx.room.Entity;
+
+@Entity
+public class OneTimeTransaction extends Transaction{
+    public OneTimeTransaction(int id,int amount,String date,String note,boolean isCredit){
+        super(id, amount, date, note, isCredit);
+    }
 }
--- app/src/main/java/app/budgetku/data/local/entity/Transaction.java
+++ app/src/main/java/app/budgetku/data/local/entity/Transaction.java
@@ -1,4 +1,65 @@
 package app.budgetku.data.local.entity;
 
-public class Transaction {
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+public abstract class Transaction {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "Amount")
+    private int amount;
+
+    @ColumnInfo(name = "Date")
+    private String date;
+
+    @ColumnInfo(name = "Note")
+    private String note;
+
+    @ColumnInfo(name = "is_credit")
+    private boolean isCredit;
+
+    public Transaction(int id, int amount,String date, String note,boolean isCredit){
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+        this.note = note;
+        this.isCredit = isCredit;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getAmount(){
+        return this.amount;
+    }
+
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public String getNote(){
+        return this.note;
+    }
+
+    public void setNote(String note){
+        this.note = note;
+    }
+
+    public boolean getIsCredit(){
+        return this.isCredit;
+    }
+
+    public void setIsCredit(boolean isCredit){
+        this.isCredit = isCredit;
+    }
 }
